995d5608e25fb7505ee171a6fe7c760289cb5cf7
Vox O <83031055+Vaokgxi@users.noreply.github.com>
2021-04-23 01:55:26 +0800

    Update NeteaseSnailReadingUnlockVip.js

--- QuantumultX/NeteaseSnailReadingUnlockVip.js
+++ QuantumultX/NeteaseSnailReadingUnlockVip.js
@@ -4,6 +4,8 @@ By yxiaocai & JO2EY & NobyDa
 Quantumult X:
 [rewrite_local]
 ^https?://p\.du\.163\.com/gain/readtime/info.json url script-response-body https://raw.githubusercontent.com/Vaokgxi/Vox/main/QuantumultX/NeteaseSnailReadingUnlockVip.js
+[mitm]
+hostname = p.du.163.com
 */
 
 var body = $response.body;
