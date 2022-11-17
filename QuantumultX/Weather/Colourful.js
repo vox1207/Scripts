b26a1a8780a059cd2e923f486d0ea02122f969a4
Vox O <83031055+vox1207@users.noreply.github.com>
2022-11-17 16:10:57 +0800

    Update Colourful.js

--- QuantumultX/Weather/Colourful.js
+++ QuantumultX/Weather/Colourful.js
@@ -253,7 +253,7 @@ function realtimeWeather() {
     hourlySkycon +=
       `${now}-${dt.getHours() + 1}时 ${mapSkycon(skycon.value)[0]}` +
       (i == 2 ? "" : "\n");     
-  
+ /* 
   let dailySkycon = "[一周预报]\n";
   for (let i = 0; i < 7; i++) {
     const skycon = daily.skycon[i];
@@ -262,7 +262,7 @@ function realtimeWeather() {
     dailySkycon +=
       `${dt.getDate()}日 ${mapSkycon(skycon.value)[0]}` +
       (i == 6 ? "" : "\n");
-    dt.setDate(dt.getDate() + 1);
+    dt.setDate(dt.getDate() + 1);*/
   }
 
   $.notify(
